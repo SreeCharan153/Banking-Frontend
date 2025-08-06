5fa585bfbbb9766754cf0acdbbcc153c7715a6a9
SreeCharan153 <223j1a4632@raghuinstech.com>
2025-08-06 13:47:24 +0530

    updted base path from local to global

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
-//const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL; // Update this to your API URL
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL; // Update this to your API URL
+//const API_BASE_URL = 'http://localhost:8000';
 export class ATMApiClient {
   private async makeRequest<T>(
     endpoint: string,

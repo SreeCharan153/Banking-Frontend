b9c4fba6af522ac0bf0cd34e5d3f127eaad050fe
SreeCharan153 <223j1a4632@raghuinstech.com>
2025-11-09 04:56:05 +0530

    updated cors

--- Backend/main.py
+++ Backend/main.py
@@ -32,8 +32,7 @@ app = FastAPI(title="Banking ATM API", version="2.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", 
-                "https://rupeewave.vercel.app/"],  # Update with your frontend URL
+    allow_origins=["https://rupeewave.vercel.app/"],  # Update with your frontend URL
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"],
